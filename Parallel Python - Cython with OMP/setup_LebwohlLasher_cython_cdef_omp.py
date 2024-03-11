ef344454e18e8ec413cb805aeee3a7dbfeb5ce8c
ghani1506 <147093492+ghani1506@users.noreply.github.com>
2024-03-11 15:10:24 +0000

    Attempt to Cythonise using OMP

---
+++ Parallel Python - Cython with OMP/setup_LebwohlLasher_cython_cdef_omp.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "LebwohlLasher_cython_cdef_omp",
+        ["LebwohlLasher_cython_cdef_omp.pyx"],
+        extra_compile_args=['-fopenmp','-v'],
+#        extra_link_args=['-fopenmp'],
+        extra_link_args=['-lgomp', '-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/13/'],
+    )
+]
+
+setup(name="LebwohlLasher_cython_cdef_omp",
+      ext_modules=cythonize(ext_modules))

2727065a913b7abe91706995a470f0be74e4322d
ghani1506 <147093492+ghani1506@users.noreply.github.com>
2024-03-13 08:59:11 +0000

    Update setup_LebwohlLasher_cython_cdef.py
    Name file changed

--- Cython cdef/setup_LebwohlLasher_cython_cdef.py
+++ Cython cdef/setup_LebwohlLasher_cython_cdef.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-
+#Changed the name of the file
 setup(name="LebwohlLasher_cython_cdef",
       ext_modules=cythonize("LebwohlLasher_cython_cdef.pyx"))
 
